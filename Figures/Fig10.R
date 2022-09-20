8f639ad3dedc5bc465a6e08459b14bc517a0778c
Cristian Roman <cromanpa94@gmail.com>
2022-09-20 11:35:37 -0700

    Fig 10

--- Figures/Fig10.R
+++ Figures/Fig10.R
@@ -1,112 +1,74 @@
-setwd("~/MEGA/Projects/2_BayClump_reviews/May2022/Figures")
 library(rio)
 library(data.table)
 library(ggplot2)
 library(ggpubr)
 library(lemon)
 library(ggthemr)
+source("https://raw.githubusercontent.com/Tripati-Lab/BayClump/dev/Functions/Calibration_BayesianNonBayesian.R")
+ggthemr('light')
+
+ds <- here::here("Analyses", "Results", "Anderson_Full")
+AndDS <- read.csv(here::here("Analyses", "Datasets", "RW_Anderson_Full.csv"))
 
 
-dirList <- list.dirs(".")[-1]
-dirList <- list.dirs()[grep("Fig9", list.dirs())]
-
-target <- dirList[1]
-TargetOutputFiles<-list.files(target,  full.names = T)
-atm <- import_list(TargetOutputFiles[[1]])
-full <- import_list(TargetOutputFiles[[2]])
+datasets <- rio::import_list(list.files(ds, full.names = T))
+datasets <- datasets[grep(" CI", names(datasets))]
+datasets <- rbindlist(datasets, idcol = "Model", fill = TRUE)
 
 
-atm <- atm[-grep( "CI", names(atm))]
-atm <- rbindlist(atm, idcol = "Model", fill = TRUE)
-
-full <- full[-grep( "CI", names(full))]
-full <- rbindlist(full, idcol = "Model", fill = TRUE)
-
-models <- unique(full$Model)
-CIsFull<- do.call(rbind, lapply(models, function(x){
-  print(x)
-  if(x != "Bayesian mixed w errors"){
-    a<-RegressionSingleCI(data = full[full$Model ==x ,], 
-                          from = 0.5, to = 13.3)[[1]]
-    cbind.data.frame(models=x, material='full', a)
-  }else{
-    mat1 <- full[full$Model ==x ,]
-    mat1[,c(2,3)] <- mat1[,c(6,7)]
-    a1<-RegressionSingleCI(data = mat1, 
-                          from = 0.5, to = 13.3)[[1]]
-    mat2 <- full[full$Model ==x ,]
-    mat2[,c(2,3)] <- mat2[,c(8,9)]
-    a2<-RegressionSingleCI(data = mat2, 
-                           from = 0.5, to = 13.3)[[1]]
-    rbind(cbind.data.frame(models=x, material=1, a1),
-    cbind.data.frame(models=x, material=2, a2))
-  }
-    
-}))
-
-CIsatm<- do.call(rbind, lapply(models, function(x){
-  print(x)
-  if(x != "Bayesian mixed w errors"){
-    a<-RegressionSingleCI(data = atm[atm$Model ==x ,], 
-                          from = 10, to = 13.3)[[1]]
-    cbind.data.frame(models=x, material='full', a)
-  }else{
-    mat1 <- atm[atm$Model ==x ,]
-    mat1[,c(2,3)] <- mat1[,c(6,7)]
-    a1<-RegressionSingleCI(data = mat1, 
-                           from = 10, to = 13.3)[[1]]
-    mat2 <- atm[atm$Model ==x ,]
-    mat2[,c(2,3)] <- mat2[,c(8,9)]
-    a2<-RegressionSingleCI(data = mat2, 
-                           from = 10, to = 13.3)[[1]]
-    rbind(cbind.data.frame(models=x, material=1, a1),
-          cbind.data.frame(models=x, material=2, a2))
-  }
-  
-}))
-
-
-Anderson_full <- RegressionSingleCI(data.frame(alpha=rnorm(1000, 0.154, 4.00E-03),
-                                          beta=rnorm(1000, 0.0391, 4.00E-04)), 
-                               0.5,13.3)
-
-Anderson_atm <- RegressionSingleCI(data.frame(alpha=rnorm(1000, 0.154, 4.00E-03),
-                                               beta=rnorm(1000, 0.0391, 4.00E-04)), 
-                                    10,13.3)
-
-Anderson <- rbind.data.frame(cbind.data.frame(Dataset='full', Anderson_full[[1]]),
-                             cbind.data.frame(Dataset='atm', Anderson_atm[[1]]))
-
-regs <- rbindlist(list(atm=CIsatm,full=CIsFull), idcol = "Dataset")
-
-
-regs$models <- factor(regs$models,
-                               levels = c("Bayesian model no errors", 
-                                          "Bayesian model with errors",
-                                          "Bayesian mixed w errors",
-                                          "Linear regression",
-                                          "Inverse linear regression",
-                                          "Deming regression", 
-                                          "York regression") ,
-                               labels = 
-                                 c("B-SL", "B-SL-E", "B-LMM", "OLS", "W-OLS", "D", "Y")
+datasets$Model <- factor(datasets$Model,
+                         levels = c("Bayesian model no errors CI", 
+                                    "Bayesian model with errors CI",
+                                    "Bayesian mixed w errors CI",
+                                    "Linear regression CI",
+                                    "Inverse linear regression CI",
+                                    "Deming regression CI", 
+                                    "York regression CI") ,
+                         labels = 
+                           c("B-SL", "B-SL-E", "B-LMM", "OLS", "W-OLS", "D", "Y")
 )
 
+datasets <- datasets[! datasets$Model == "B-LMM",]
 
-regs$Dataset <- factor(regs$Dataset, c("full", "atm"), labels = c("Anderson - Full", "Anderson -  ATM"))
-Anderson$Dataset <- factor(Anderson$Dataset, c("full", "atm"), labels = c("Anderson - Full", "Anderson -  ATM"))
+datasets$material <- ifelse(is.na(datasets$material), "Full", datasets$material)
 
-p1 <- ggplot(data=regs) +
-   geom_ribbon(data=regs,aes(x=x, y = median_est, ymin = ci_lower_est,
-                            ymax = ci_upper_est, group=material),color=NA,fill="orange",
+#Subset per material
+
+#range1 <- range(AndDS[AndDS$Material == 1,"Temperature"])
+#range2 <- range(AndDS[AndDS$Material == 2,"Temperature"])
+
+#Mat1 <- datasets[datasets$material == 1,]
+#Mat2 <- datasets[datasets$material == 2,]
+#MatFull <- datasets[datasets$material == "Full",]
+
+#Mat1 <- Mat1[between(unlist(Mat1[,2]), range1[1], range1[2] ),]
+#Mat2 <- Mat2[between(unlist(Mat2[,2]), range2[1], range2[2] ),]
+#datasets <- rbind.data.frame(MatFull, Mat1, Mat2)
+
+##
+
+
+
+Anderson <- RegressionSingleCI(data.frame(alpha=rnorm(1000, 0.154, 4.00E-03),
+                                          beta=rnorm(1000, 0.0391, 4.00E-04)), 
+                               0.5,13.3)
+regs <- datasets
+colnames(regs)[2] <- "Temperature"
+
+
+p1 <- 
+  ggplot(data=regs) +
+  geom_ribbon(data=regs,aes(x=Temperature, y = D47_median_est, ymin = D47_ci_lower_est,
+                            ymax = D47_ci_upper_est, fill = material), fill = "grey",
               alpha = 0.8)+
-  geom_line(data=regs,aes(x=x, y = median_est,group=material), color="blue")+
-  geom_line(data=Anderson,
+  geom_ribbon(data=Anderson[[1]],aes(x=x, y = median_est, ymin = ci_lower_est,
+                                     ymax = ci_upper_est), color=NA,
+              alpha = 0.8)+
+  geom_line(data=regs,aes(x=Temperature, y = D47_median_est), color = ' black')+
+  geom_line(data=Anderson[[1]],
             aes(x=x, y = median_est), color="red", lty="dashed")+
-  geom_ribbon(data=Anderson,aes(x=x, y = median_est, ymin = ci_lower_est,
-                                     ymax = ci_upper_est),color=NA,fill="grey",
-              alpha = 0.8)+
-  facet_grid(Dataset~models, scales = "free_y") +
+
+  facet_grid(.~Model) +
   ylab(expression(Delta["47"]*" (‰)" ))+ 
   xlab(expression(paste(10^6, " / T"^2, "(Temperature in °K)")))+ 
   guides(color="none", fill='none')+ 
@@ -120,22 +82,22 @@ p1 <- ggplot(data=regs) +
         axis.line.x.bottom=element_line(color="black", size=0.1),
         axis.line.y.left=element_line(color="black", size=0.1),
         text = element_text(size=15))+
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 7))+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))+
   theme(axis.text.x = element_text(colour="black"), 
         axis.text.y = element_text(colour="black"))+ 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"),
         axis.ticks = element_line(colour = "black")
-  )+ theme(text = element_text(size = 17)) 
+  ) + theme(text = element_text(size = 17))
 
 
-pdf('Plots/Fig10.pdf', 10, 5)
+pdf(here::here("Figures","Plots",'Fig10.pdf'), 10, 5)
 print(p1)
 dev.off()
 
 
-jpeg('Plots/Fig10.jpg', 15, 5, units = "in", res=300)
+jpeg(here::here("Figures","Plots",'Fig10.jpg'), 15, 5, units = "in", res=300)
 print(p1)
 dev.off()
 
